2fac663a3b78958d99ed41a7d57e6c559a12ee2c
exlted <exltedofdrg@gmail.com>
2016-03-22 10:27:46 -0400

    Added early and late update and render delegates to Update() and Render()

--- Roguelike/PL2D/PL2D/GameLoopFunctions.cs
+++ Roguelike/PL2D/PL2D/GameLoopFunctions.cs
@@ -7,23 +7,39 @@ using System.Threading.Tasks;
 
 namespace PL2D
 {
+    delegate void addUpdate();
+    delegate void addRender(SpriteBatch spriteBatch);
+
     static class GameLoopFunctions
     {
         public static List<Entity> Updatable = new List<Entity>();
         public static List<Cell> Renderable = new List<Cell>();
         public static InputState inputState = new InputState();
+        public static addUpdate earlyUpdate;
+        public static addUpdate lateUpdate;
+        public static addRender earlyRender;
+        public static addRender lateRender;
+
 
         public static void Update()
         {
+            if(earlyUpdate !=  null)
+                earlyUpdate();
             inputState.Update();
             foreach (Entity E in Updatable)
                 E.Update();
+            if (lateUpdate != null)
+                lateUpdate();
         }
 
         public static void Render(SpriteBatch spriteBatch)
         {
+            if(earlyRender != null)
+                earlyRender(spriteBatch);
             foreach (Cell C in Renderable)
                 C.Draw(spriteBatch);
+            if (lateRender != null)
+                lateRender(spriteBatch);
         }
     }
 }
